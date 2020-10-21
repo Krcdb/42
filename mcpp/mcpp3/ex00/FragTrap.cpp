15341ad81e4aa283f01395df4c22762e8855b45d
krcdb <medo.mrt@gmail.com>
2020-10-21 16:25:19 +0200

    cpp02 ex00

---
+++ mcpp/mcpp3/ex00/FragTrap.cpp
@@ -0,0 +1,179 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: memartin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/04 18:03:04 by memartin          #+#    #+#             */
+/*   Updated: 2020/10/18 18:12:06 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap(void) :
+	_name("default"),
+	_hp(100),
+	_hpMax(100),
+	_ep(100),
+	_epMax(100),
+	_level(1),
+	_meleeAttackDamage(30),
+	_rangedAttackDamage(20),
+	_armorDamageReduction(5)
+{
+	say("created !");
+	say("Hey everybody! Check out my package!");
+}
+
+FragTrap::FragTrap(std::string name):
+	_name(name),
+	_hp(100),
+	_hpMax(100),
+	_ep(100),
+	_epMax(100),
+	_level(1),
+	_meleeAttackDamage(30),
+	_rangedAttackDamage(20),
+	_armorDamageReduction(5)
+	
+{
+	say("created !");
+	say("Hey everybody! Check out my package!");
+}
+
+FragTrap::FragTrap(const FragTrap &other)
+{
+	*this = other;
+	say("copied !");
+	say("This time it'll be awesome, I promise!");
+}
+
+
+FragTrap::~FragTrap()
+{
+	say("Aww! Come on!");
+	say("destroyed !");
+}
+
+FragTrap&
+FragTrap::operator =(const FragTrap &other)
+{
+	if (this != &other)
+	{
+		this->_name = other._name;
+		this->_hp = other._hp;
+		this->_hpMax = other._hpMax;
+		this->_ep = other._ep;
+		this->_epMax = other._epMax;
+		this->_level = other._level;
+		this->_meleeAttackDamage = other._meleeAttackDamage;
+		this->_rangedAttackDamage = other._rangedAttackDamage;
+		this->_armorDamageReduction = other._armorDamageReduction;
+	}
+	say("assigned !");
+	say("Recompiling my combat code!");
+	return (*this);
+}
+
+std::ostream&
+FragTrap::say(void)
+{
+	return (std::cout << this->_name << " : ");
+}
+
+std::ostream&
+FragTrap::say(std::string output)
+{
+	return (say() << output << std::endl);
+}
+
+void
+FragTrap::rangedAttack(std::string const &target)
+{
+	say() << "I'm a tornado of death and bullets! (" << target << " receive " << 
+		this->_rangedAttackDamage << " ranged  damages !" << std::endl;
+}
+
+void
+FragTrap::meleeAttack(std::string const &target)
+{
+	say() << "Take that! (" << target << " receive " << 
+		this->_meleeAttackDamage << " melee damages !" << std::endl;
+}
+
+void
+FragTrap::takeDamage(unsigned int amount)
+{
+	int	damage;
+	damage = amount - this->_armorDamageReduction;
+	
+	if (damage <= 0 && this->_hp != 0)
+		say("Nice try !");
+	else if (damage >= this->_hp)
+	{
+		say("Woah! Oh! Jeez!") << this->_name << " is dead ! See you space FR4G-TP" << std::endl;
+		this->_hp = 0;
+	}
+	else
+	{
+		say("Ouch !") << this->_name << " received " << damage << " damages !" << std::endl;
+		this->_hp -= damage;
+	}
+}
+
+void
+FragTrap::beRepaired(unsigned int amount)
+{
+	int heal;
+
+	if (amount > (this->_hpMax - this->_hp))
+		heal = this->_hpMax - this->_hp;
+	else
+		heal = amount;
+	if (this->_hp == 100)
+		say("I'm full bro !");
+	else
+	{
+		say("Yay ! Healing !") << this->_name << " healed for " << heal << " hp !" << std::endl;
+		this->_hp += heal;
+	}
+}
+
+void
+FragTrap::vaultHunter_dot_exe(std::string const &target)
+{
+	if (this->_ep < 25)
+	{
+		say("Oy ! I need more EP !");
+		return ;
+	}
+	static const std::string attacks[] = {
+		"Blightbot",
+		"Mechromagician",
+		"Miniontrap",
+		"Knock-knock jokes",
+		"Shhhh...trap"
+	};
+
+	static const int damages[] = {
+		32,
+		46,
+		15,
+		0,
+		2
+	};
+
+	int	random = rand() % 5;
+
+	say() << "use " << attacks[random] << std::endl;
+	say() << "Take that! (" << target << " receive " << 
+		damages[random] << " damages !" << std::endl;
+}
+
+void
+FragTrap::displayStatus(void)
+{
+	say() << "current HP : " << this->_hp << " | current EP : " << this->_ep << std::endl;
+}
--- mcpp/mcpp3/ex00/FragTrap.hpp
+++ mcpp/mcpp3/ex00/FragTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: memartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/29 17:38:00 by memartin          #+#    #+#             */
-/*   Updated: 2020/09/29 17:56:57 by memartin         ###   ########.fr       */
+/*   Updated: 2020/10/18 18:00:44 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,40 @@
 
 # include <string>
 # include <iostream>
+# include <cstdlib>
 
 class FragTrap
 {
 	private:
 		std::string		_name;
-		unsigned int	_hp = 100;
-		unsigned int	_hpMax = 100;
-		unsigned int	_ep = 100;
-		unsigned int	_epMax = 100;
-		unsigned int	_level = 1;
-		unsigned int	_meleeAttackDamage = 30;
-		unsigned int	_rangedAttackDamage = 20;
-		unsigned int	_armorDamageReduction = 5;
+		int				_hp = 100;
+		int				_hpMax = 100;
+		int				_ep = 100;
+		int				_epMax = 100;
+		int				_level = 1;
+		int				_meleeAttackDamage = 30;
+		int				_rangedAttackDamage = 20;
+		int				_armorDamageReduction = 5;
 	
+	protected:
+		std::ostream&	say(void);
+		std::ostream&	say(std::string output);
+
 	public:
-		FragTrap();
+		FragTrap(void);
 		FragTrap(std::string name);
-		virtual ~FragTrap();
+		FragTrap(const FragTrap &other);
+		virtual ~FragTrap(void);
+
+		FragTrap &operator =(const FragTrap &other);
+
 		void			rangedAttack(std::string const &target);
 		void			meleeAttack(std::string const &target);
 		void			takeDamage(unsigned int amount);
 		void			beRepaired(unsigned int amount);
 		void			vaultHunter_dot_exe(std::string const &target);
+
+		void			displayStatus(void);
 };
 
 #endif
